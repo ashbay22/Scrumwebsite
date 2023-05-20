30edb82f15117923c1e689724fa5c33c83101adf
ashbay22 <114266833+ashbay22@users.noreply.github.com>
2023-05-20 04:26:50 -0700

    Add files via upload

---
+++ scrum.js
@@ -0,0 +1,5 @@
+const menuBtn = document.getElementById('navbar_toggle');
+const check = document.getElementById('navbar-menu')
+menuBtn.addEventListener('click', () => {
+    check.style.bottom = "0";
+}) 
